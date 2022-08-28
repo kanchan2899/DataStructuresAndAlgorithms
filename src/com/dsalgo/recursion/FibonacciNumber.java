79e464a3f568ffc03732f45b35be2cb530069081
kanchan2899 <kanchan2899@gmail.com>
2022-08-28 22:24:53 +0100

    Recursion programs

--- src/com/dsalgo/recursion/FibonacciNumber.java
+++ src/com/dsalgo/recursion/FibonacciNumber.java
@@ -2,12 +2,18 @@ package com.dsalgo.recursion;
 
 public class FibonacciNumber {
     public static void main(String[] args) {
-        int n = 6;
-        System.out.println(fibonacci(6));
+        int n = 4;
+        System.out.println(fibonacci(n));
+        System.out.println(fibonacciFormula(50));
     }
 
+    private static long fibonacciFormula(int n){
+         // return  Math.pow(((1 + Math.sqrt(5)) / 2), n);
+        // return (int) ((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+        return (long) ((Math.pow(((1 + Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+    }
     private static int fibonacci(int n) {
-        if(n == 1 || n == 0){
+        if(n < 2){
             return n;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
