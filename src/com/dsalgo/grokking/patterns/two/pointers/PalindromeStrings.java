12c649b93c2f04786a78f9523e3aa5ae4cc53cf4
kanchan2899 <kanchan2899@gmail.com>
2023-03-09 21:06:19 +0000

    more programs

--- src/com/dsalgo/grokking/patterns/two/pointers/PalindromeStrings.java
+++ src/com/dsalgo/grokking/patterns/two/pointers/PalindromeStrings.java
@@ -18,6 +18,9 @@ public class PalindromeStrings {
      * Using Bruteforce: Reverse the input string and compare with equals method if they
      * are equal. If so, return true. Else return false.
      *
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     *
      * @param a, input string
      * @return true if s is palindrome, else false
      */
@@ -30,11 +33,20 @@ public class PalindromeStrings {
     }
 
     /**
-     * Using Two-Pointer approach: Initialize start pointer to 0 (start index)
-     * and end point to s.length() - 1 (last index). Start a loop until start <= end
-     * If s.charAt(start) == s.charAt(end), increment start and decrement end. Otherwise, break
-     * out of the loop and return false. If traversed all the chars in the string till mid of the
-     * string, return true.
+     * Using Two-Pointer approach: Traverse the array from the start and the end at the same time
+     * until you reach the middle of the string. Compare the char at start and end index.
+     * Return true if all char at start and end are equal, else return false.
+     *
+     * Algorithm:
+     *
+     * 1. Initialize start pointer to 0 (start index) and end point to s.length() - 1 (last index).
+     * 2. Start a loop until start <= end
+     * 3. If s.charAt(start) == s.charAt(end), increment start and decrement end.
+     * 4. Otherwise, break out of the loop and return false.
+     * 5. If traversed all the chars in the string till mid of the string, return true.
+     *
+     * Time complexity: O(n) - logically traversing till mid of the array, not its length
+     * Space complexity: O(1)
      *
      * @param s, input string
      * @return true if s is palindrome, else false
@@ -42,7 +54,7 @@ public class PalindromeStrings {
     private static boolean isPalindrome2(String s) {
         int i = 0;
         int j = s.length() - 1;
-        while(i <= j) {
+        while(i < j) {
             if(s.charAt(i) == s.charAt(j)){
                 i++;
                 j--;
